830109f4a9c1a75103c4fe502003e58fa5606868
Matheus dos Santos Souza <ssouza.matheus@outlook.com>
2024-01-27 14:21:41 -0300

    feat: finished transactions model

--- backend/src/models/transactionModel.ts
+++ backend/src/models/transactionModel.ts
@@ -1,6 +1,11 @@
 import pool from "../config/dbConnect";
 
 class TransactionModel {
+  async getTransaction(id: string) {
+    const [rows] = await pool.execute('SELECT * FROM transactions where id = ?', [id]);
+    return rows;
+  }
+
   async getAllTransactions() {
     const [rows] = await pool.execute('SELECT * FROM transactions');
     return rows;
@@ -13,6 +18,19 @@ class TransactionModel {
       'INSERT INTO transactions (client, description, transactionType, value, date) values (?,?,?,?,?)',
       [client, description, transactionType, value, date] 
     );
+  }
+
+  async updateTransaction(id: string, client:string, description: string, transactionType: string, value: number){
+    const date = new Date();
+
+    await pool.execute(
+      'UPDATE transactions SET client = ?, description = ?, transactionType = ?, value = ?, date = ? WHERE id = ?',
+      [client, description,transactionType,value,date,id]
+    );
+  }
+
+  async deleteTransaction(id: string){
+    await pool.execute('DELETE FROM transactions WHERE id = ?', [id]);
   }
 }
 
